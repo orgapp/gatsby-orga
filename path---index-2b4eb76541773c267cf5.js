3c7abb3d530d66cc9e4cc5ea770229e5f19d0c43
Xiaoxing Hu <dawnstar.hu@gmail.com>
2018-02-22 12:24:45 +1300

    Updates

--- path---index-2b4eb76541773c267cf5.js
+++ path---index-2b4eb76541773c267cf5.js
@@ -1,2 +1,2 @@
-webpackJsonp([0x81b8806e4260],{384:function(t,e){t.exports={data:{site:{siteMetadata:{title:"Gatsby Orga Starter"}},allOrga:{edges:[{node:{fields:{slug:"/another_org_file/"},meta:{title:"Another Org File"}}},{node:{fields:{slug:"/syntax/"},meta:{title:"Orga with Gatsby",date:"2018-01-01"}}}]}},pathContext:{}}}});
+webpackJsonp([0x81b8806e4260],{385:function(t,e){t.exports={data:{site:{siteMetadata:{title:"Gatsby Orga Starter"}},allOrga:{edges:[{node:{fields:{slug:"/another_org_file/"},meta:{title:"Another Org File"}}},{node:{fields:{slug:"/syntax/"},meta:{title:"Orga with Gatsby",date:"2018-01-01"}}}]}},pathContext:{}}}});
 //# sourceMappingURL=path---index-2b4eb76541773c267cf5.js.map
