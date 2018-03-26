8c77d0ea4cf117ae2682a419c4ab6f8a2bfcb96d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-03-26 03:33:34 +0000

    Deploy xiaoxinghu/gatsby-orga to github.com/xiaoxinghu/gatsby-orga.git:gh-pages

--- path---another-org-file-4ee43d69c6e06fe9f34e.js
+++ path---another-org-file-4ee43d69c6e06fe9f34e.js
@@ -1,2 +1,2 @@
-webpackJsonp([0xacf543a57f4d],{384:function(a,e){a.exports={data:{orga:{html:'<div>Here is another org file.</div><div><a href="/syntax/">go back</a></div>',meta:{title:"Another Org File"}}},pathContext:{slug:"/another_org_file/"}}}});
+webpackJsonp([0xacf543a57f4d],{383:function(a,e){a.exports={data:{orga:{html:'<div>Here is another org file.</div><div><a href="/syntax/">go back</a></div>',meta:{title:"Another Org File"}}},pathContext:{slug:"/another_org_file/"}}}});
 //# sourceMappingURL=path---another-org-file-4ee43d69c6e06fe9f34e.js.map
